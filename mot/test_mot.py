b119238450a719399094fb2c2e8463c6509869f8
fretchen <fred.jendrzejewski@gmail.com>
2023-10-14 20:52:17 +0200

    First attempt (#14)
    * First attempt
    
    * Lint everything
    
    * Correct OS
    
    * Update spooler_tests.yml
    
    * Update spooler_tests.yml
    
    * Update spooler_tests.yml
    
    * Update spooler_tests.yml
    
    * Update spooler_tests.yml
    
    * Update spooler_tests.yml
    
    * Update spooler_tests.yml
    
    * Update spooler_tests.yml
    
    * Update spooler_tests.yml
    
    * Update spooler_tests.yml
    
    * Update spooler_tests.yml
    
    * Update spooler_tests.yml
    
    * Update spooler_tests.yml
    
    * Now also prepare everything for the test itself
    
    * Update spooler_tests.yml
    
    * Add stuff for pip
    
    * Update spooler_tests.yml
    
    * Update requirements-dev.txt
    
    * The standard config
    
    * Update spooler_tests.yml
    
    * Update spooler_tests.yml
    
    * Add the secret

--- mot/test_mot.py
+++ mot/test_mot.py
@@ -3,7 +3,6 @@ Test module for the mot file.
 """
 
 from typing import Union
-import numpy as np
 
 import pytest
 from pydantic import ValidationError
@@ -228,7 +227,7 @@ def test_number_experiments() -> None:
     job_payload = {}
     for ii in range(n_exp):
         job_payload[f"experiment_{ii}"] = inst_dict
-    job_id = 1
+    job_id = "1"
     with pytest.raises(AssertionError):
         data = run_json_circuit(job_payload, job_id)
 
