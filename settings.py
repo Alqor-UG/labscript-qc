29561c9a43c17bebceee758e7e2446b263ffaa74
fretchen <fred.jendrzejewski@gmail.com>
2023-11-11 08:09:15 +0100

    Update settings.py

---
+++ settings.py
@@ -0,0 +1,14 @@
+"""
+In this file, we will define all the settings for the maintainer. Any instance of labscript-qc has to change this file
+to suit their needs. The maintainer will be responsible for updating the backends on the storage provider.
+"""
+
+from mot.config import spooler_object as mot_spooler
+
+# valid storage types are: "local", "mongodb" and "dropbox"
+storage_type = "mongodb"
+
+# configure the backends
+backends = {
+    "mot": mot_spooler,
+}
